1cc25c8bcac50f7ec3d5185670f772d9a6281ef0
Cryyli <cry.yyli@gmail.com>
2023-04-24 15:02:24 -0700

    Display and hide result message

--- script.js
+++ script.js
@@ -58,17 +58,33 @@ function calculateCatFoodAmount(weight, activityLevel, mealType) {
     return foodAmount.toFixed(2);
   }
 
-  const form = document.querySelector('#cat-food-form');
-  const resultMessage = document.querySelector('#cat-food-result');
+  const submitBtn = document.querySelector('#submit-btn');
 
-  form.addEventListener('submit', (event) => {
+  submitBtn.addEventListener('click', (event) => {
     event.preventDefault(); // prevent form submission
+    const form = document.querySelector('#cat-food-form')
+    const resultContainer = document.querySelector('#cat-food-result');
+
     const weight = parseFloat(form.elements['cat-weight'].value);
     const activityLevel = form.elements['activity-level'].value;
     const mealType = form.elements['meal-type'].value;
     const foodAmount = calculateCatFoodAmount(weight, activityLevel, mealType);
+
     const name = form.elements['name'].value;
-    
-    resultMessage.innerHTML = `<div class="my-5 py-5"> Hi, I am <strong>${name}</strong>.<br/> I weigh <div class="number number-style">${weight} lbs</div> and live a <strong>${activityLevel}</strong> lifestyle.<br/> Because I eat <strong>${mealType}</strong> food, I should eat <div class="number number-style">${foodAmount}</div> cups of food per day.</div>`;
-    resultMessage.style.display = 'block';
+    const resultMessage = `<div class="my-5 py-5"> Hi, I am <strong>${name}</strong>.<br/> I weigh <div class="number number-style">${weight} lbs</div> and live a <strong>${activityLevel}</strong> lifestyle.<br/> Because I eat <strong>${mealType}</strong> food, I should eat <div class="number number-style">${foodAmount}</div> cups of food per day.</div>`;
+    form.style.display='none';
+    resultContainer.style.display = 'block';
+    resultContainer.innerHTML = resultMessage;
+  });
+
+  const resetBtn = document.querySelector('#reset-btn');
+
+  resetBtn.addEventListener('click', (event) => {
+    const form = document.querySelector('#cat-food-form');
+    const resultContainer = document.querySelector('#cat-food-result');
+  
+    // clear result message
+    resultContainer.innerHTML = '';
+    resultContainer.style.display = 'none';
+    form.style.display = 'block';
   });
